b1ccbf986749bd6c691953f8c2e5c1fad26c788c
armac7 <113379340+armac7@users.noreply.github.com>
2023-10-03 19:05:08 -0500

    Testing Parallax

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,12 @@
+let width = screen.width;
 $('document').ready(function() {
     setTimeout(() => {
         AOS.init();
     }, 120);
+
+    if (width >= 768) {
+        $('#team-header').addClass("team-parallax");
+    } else {
+        $('#team-header').removeClass("team-parallax");
+    }
 });
